9645f626a9db08829b801ad55b8110b20ef501c0
upendrasingh <164949459+upendra512@users.noreply.github.com>
2024-05-09 20:40:34 +0530

    Update heatmap.py

--- TASK_1/SL_2/heatmap.py
+++ TASK_1/SL_2/heatmap.py
@@ -1,1 +1,16 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
 
+# read two input images
+imgL = cv2.imread('left.png',0)
+imgR = cv2.imread('right.png',0)
+
+# Initiate and StereoBM object
+stereo = cv2.StereoBM_create(numDisparities=128,blockSize=21)
+
+# compute the disparity map
+disparity = stereo.compute(imgL,imgR)
+disparity1 = stereo.compute(imgR,imgL)
+plt.imshow(disparity)
+plt.show()
